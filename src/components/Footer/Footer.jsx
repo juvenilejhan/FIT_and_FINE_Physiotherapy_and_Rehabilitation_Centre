77c32f4b600313c62b82818c71e87b813ee49322
juvenilejhan <juvenilejhan@gmail.com>
2026-07-05 20:42:09 +0600

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import "./Footer.css";
 
 export default function Footer({ facebook }) {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <p>Copyright ©️ FIT & FINE Physiotherapy & Rehabilitation Centre</p>
+      <p>
+        Copyright {currentYear} ©️ FIT & FINE Physiotherapy & Rehabilitation
+        Centre
+      </p>
       <a href={facebook} target="_blank" rel="noreferrer">
         Facebook Page
       </a>
